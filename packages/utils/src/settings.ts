b63bb8a1936f83ac71572d4e0aa3769315024edb
m7om7 <54171860+m7om7@users.noreply.github.com>
2025-05-13 15:42:27 +0200

    Update settings.ts

--- packages/utils/src/settings.ts
+++ packages/utils/src/settings.ts
@@ -290,7 +290,7 @@ export const Settings = cleanEnv(process.env, {
     desc: 'Default regex include pattern',
   }),
   DEFAULT_REGEX_SORT_PATTERNS: multipleRegex({
-    default: undefined,
+    default: .*FLUX.*,
     desc: 'Default regex sort patterns',
   }),
 
